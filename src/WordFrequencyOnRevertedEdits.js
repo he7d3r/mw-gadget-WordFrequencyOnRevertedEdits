271318cb32d59b9590db90f06359f5880c0a7a67
Helder <he7d3r@gmail.com>
2014-09-20 11:22:29 -0300

    Remove tracking link (only useful on wiki)

--- src/WordFrequencyOnRevertedEdits.js
+++ src/WordFrequencyOnRevertedEdits.js
@@ -2,7 +2,6 @@
  * Generate a list of words by frequency based on reverted edits
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/WordFrequencyOnRevertedEdits.js]] ([[File:User:Helder.wiki/Tools/WordFrequencyOnRevertedEdits.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true, laxbreak: true, devel:true, forin: false*/
 /*global jQuery, mediaWiki */
