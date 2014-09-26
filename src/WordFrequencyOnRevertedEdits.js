f2b6f9eb40888cd295f14e4b078f3d3822deb8ff
Helder <he7d3r@gmail.com>
2014-09-26 15:21:26 -0300

    Define JSHint configuration on .jshintrc

--- src/WordFrequencyOnRevertedEdits.js
+++ src/WordFrequencyOnRevertedEdits.js
@@ -3,8 +3,6 @@
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
-/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true, laxbreak: true, devel:true, forin: false*/
-/*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
 
